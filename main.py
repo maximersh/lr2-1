18191f6fc243ad8566d64ca50d75c1380ff5b876
Maxim Ersh <maximers6e@gmail.com>
2024-10-02 20:49:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+n = int(input("Школьники:"))
+k = int(input("Яблоки:"))
+sk_int = k // n
+ap_float = k % n
+print("Яблок достанется каждому школьнику:", sk_int)
+print("Яблок останется в корзине:", ap_float)
